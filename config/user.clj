136050d22d20a01fcb49063ca636c2ec94e2c325
Abner Terribili <abner.terribili@gmail.com>
2020-01-21 11:07:54 -0300

    Add tools.deps

---
+++ config/user.clj
@@ -0,0 +1,4 @@
+;; File is required for project.clj :repl-options {:init user} to work
+(ns user
+  (:require
+   [clojure.tools.namespace.repl :refer [refresh refresh-all]]))
--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
 (defproject today "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Hola"
+  :url "https://github.com/aterribili/today"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns today.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.cognitect.aws/api "0.8.408"]
+                 [com.cognitect.aws/s3 "780.2.583.0"]
+                 [com.cognitect.aws/endpoints "1.1.11.692"]
+                 [org.clojure/tools.namespace "0.3.1"]]
+  :profiles {:dev {:source-paths   ["config"]}}
+  :repl-options {:init-ns user})
